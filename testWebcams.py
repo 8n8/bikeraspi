08c900788b3cf262cf12b71ac156f7cb0ef2a921
True Ghiassi <true@ghiassitrio.co.uk>
2018-08-15 16:10:51 +0100

    Before changing from pygame to opencv in webcam test script.

--- testWebcams.py
+++ testWebcams.py
@@ -7,57 +7,31 @@ pygame.init()
 pygame.camera.init()
 
 
+@profile
 def main():
     camList = pygame.camera.list_cameras()
 
-    time.sleep(0.1)
-
-    if len(camList) != 3:
-        print "The list of cameras doesn't have exactly three elements."
+    if len(camList) != 1:
+        print "The list of cameras doesn't have exactly two elements."
         return
     print "got cam names"
 
-    # handles = [
-    #     pygame.camera.Camera(camName, (640, 480)) for camName in camList]
+    handles = [
+        pygame.camera.Camera(camName, (640, 480)) for camName in camList]
 
-    handles = [pygame.camera.Camera(camName, (640, 480)) for camName in camList]
-
-    print "got handles"
-    for handle in handles:
-        print "starting handle"
-        handle.start()
-
-    h0 = handles[0]
-    h1 = handles[1]
-    h2 = handles[2]
-
-    print "handles started"
-
-    time.sleep(0.1)
+    for h in handles:
+        h.start()
 
     repetitions = 100
 
     a = time.time()
 
-    for _ in range(repetitions):
-        print "top of loop"
+    for j in range(repetitions):
+        print "top of loop" + str(j)
 
-        im0 = h0.get_image()
-        pygame.image.save(im0, "testIm0.jpg")
-
-        print "done pic 1"
-        time.sleep(0.3)
-
-        im1 = h1.get_image()
-        print "b"
-        pygame.image.save(im1, "testIm1.jpg")
-
-        print "done pic 2"
-
-        im2 = h2.get_image()
-        pygame.image.save(im2, "testIm2.jpg")
-
-        print "done pic 3"
+        for i, h in enumerate(handles):
+            im = h.get_image()
+            pygame.image.save(im, "testIm" + str(i) + ".jpg")
 
     print (time.time() - a) / repetitions
 
